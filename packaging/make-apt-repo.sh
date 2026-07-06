49f13b2d7c0fc71429ebdc0dc87c0c56f442153d
シェンチャン ケ <98302729+SkyWalkerAMD@users.noreply.github.com>
2026-07-06 16:49:11 +0800

    Add files via upload

--- packaging/make-apt-repo.sh
+++ packaging/make-apt-repo.sh
@@ -6,13 +6,13 @@ cp ./*.deb apt-repo/pool/
 cd apt-repo
 dpkg-scanpackages --arch amd64 pool /dev/null > dists/stable/main/binary-amd64/Packages
 gzip -9c dists/stable/main/binary-amd64/Packages > dists/stable/main/binary-amd64/Packages.gz
-cat > dists/stable/Release <<REL
-Origin: msr-sck
-Suite: stable
-Codename: stable
-Architectures: amd64
-Components: main
-Date: $(date -Ru)
-REL
+apt-ftparchive \
+  -o APT::FTPArchive::Release::Origin=msr-sck \
+  -o APT::FTPArchive::Release::Suite=stable \
+  -o APT::FTPArchive::Release::Codename=stable \
+  -o APT::FTPArchive::Release::Architectures=amd64 \
+  -o APT::FTPArchive::Release::Components=main \
+  release dists/stable > /tmp/Release.tmp
+mv /tmp/Release.tmp dists/stable/Release
 printf '<h1>msr-sck apt repository</h1><p>See <a href="https://github.com/SkyWalkerAMD/msr-sck">GitHub</a> for install instructions.</p>' > index.html
 echo "apt repo ready under apt-repo/"
